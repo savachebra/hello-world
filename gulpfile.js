32a8543fc8c41adb254eb6c0725247218e0a7601
savachebra <savachebra@gmail.com>
2016-02-15 16:28:16 +0200

    so so

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
 var gulp = require('gulp');
-// Gulp dependencies go here
 var babel = require('gulp-babel');
 
 gulp.task('default', function() {
-  // Gulp tasks go here
   // Node source
   gulp.src("es6/**/*.js")
-    .pipe(babel())
+    .pipe(babel({presets: ['es2015']}))
     .pipe(gulp.dest("dist"));
   // browser source
   gulp.src("public/es6/**/*.js")
-    .pipe(babel())
+    .pipe(babel({presets: ['es2015']}))
     .pipe(gulp.dest("public/dist"));
 });
