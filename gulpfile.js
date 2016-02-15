4d5d6ee4f5444b3a98f87f97d893888042ea58dd
savachebra <savachebra@gmail.com>
2016-02-15 16:48:35 +0200

    so so

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
+  // Run ESLint
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
   // Node source
   gulp.src("es6/**/*.js")
     .pipe(babel({presets: ['es2015']}))
--- public/es6/test.js
+++ public/es6/test.js
@@ -1,14 +1,14 @@
 // es6 feature: block-scoped "let" declaration
 let sentences = [
     { subject: 'PubJavaScript', verb: 'is', object: 'great' },
-    { subject: 'PubElephants', verb: 'are', object: 'large' },
-];
+    { subject: 'PubElephants', verb: 'are', object: 'large' }
+]
 // es6 feature: object destructuring
-function say({ subject, verb, object }) {
+function say ({ subject, verb, object }) {
     // es6 feature: template strings
-    console.log(`${subject} ${verb} ${object}`);
+  console.log(`${subject} ${verb} ${object}`)
 }
 // es6 feature: for..of
-for(let s of sentences) {
-    say(s);
+for (let s of sentences) {
+  say(s)
 }
