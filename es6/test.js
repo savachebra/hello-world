8d2d083bd5956c1346ea42a33ffc73d82d6f23a7
savachebra <savachebra@gmail.com>
2016-02-15 16:17:30 +0200

    so so

---
+++ es6/test.js
@@ -0,0 +1,14 @@
+// es6 feature: block-scoped "let" declaration
+let sentences = [
+    { subject: 'JavaScript', verb: 'is', object: 'great' },
+    { subject: 'Elephants', verb: 'are', object: 'large' },
+];
+// es6 feature: object destructuring
+function say({ subject, verb, object }) {
+    // es6 feature: template strings
+    console.log(`${subject} ${verb} ${object}`);
+}
+// es6 feature: for..of
+for(let s of sentences) {
+    say(s);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 // Gulp dependencies go here
+var babel = require('gulp-babel');
 
 gulp.task('default', function() {
-    // Gulp tasks go here
+  // Gulp tasks go here
+  // Node source
+  gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+  // browser source
+  gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 });
